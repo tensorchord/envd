0c86687b53a0fbda94928ddd8cfb38bf347ce3a3
Ce Gao <ce.gao@outlook.com>
2022-04-21 17:10:16 +0800

    feat(build): Support -t and use python:3.8 as base image
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- pkg/lang/ir/stmt.go
+++ pkg/lang/ir/stmt.go
@@ -15,15 +15,13 @@
 package ir
 
 import (
-	"fmt"
-
 	"github.com/moby/buildkit/client/llb"
 )
 
 var Stmt llb.State
 
 func BaseStmt(os, language string) {
-	base := llb.Image(fmt.Sprintf("docker.io/library/%s:%s", language, os))
-	run := base.Run(llb.Shlex("ls -la"))
-	Stmt = run.State
+	// TODO(gaocegege): Support multi os and languages.
+	base := llb.Image("docker.io/library/python:3.8")
+	Stmt = base
 }
