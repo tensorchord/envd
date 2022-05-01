b0d47fe3137fa3431e57caa101a30b1f8403fed5
Ce Gao <ce.gao@outlook.com>
2022-05-01 13:55:41 +0800

    chore: autocomplete bash and zsh (#93)
    * chore: Fix autocomplete
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * fix: Update
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

---
+++ pkg/autocomplete/bash.go
@@ -0,0 +1,86 @@
+package autocomplete
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/cockroachdb/errors"
+
+	"github.com/tensorchord/MIDI/pkg/util/fileutil"
+)
+
+var autocompleteBASH = `
+#! /bin/bash
+$PROG=midi
+: ${PROG:=$(basename ${BASH_SOURCE})}
+
+_cli_bash_autocomplete() {
+  if [[ "${COMP_WORDS[0]}" != "source" ]]; then
+    local cur opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ "$cur" == "-"* ]]; then
+      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
+    else
+      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+    fi
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
+
+complete -o bashdefault -o default -o nospace -F _cli_bash_autocomplete $PROG
+unset PROG
+`
+
+func InsertBashCompleteEntry() error {
+	var path string
+	if runtime.GOOS == "darwin" {
+		path = "/usr/local/etc/bash_completion.d/midi"
+	} else {
+		path = "/usr/share/bash-completion/completions/midi"
+	}
+	dirPath := filepath.Dir(path)
+
+	dirPathExists, err := fileutil.DirExists(dirPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed checking if %s exists", dirPath)
+	}
+	if !dirPathExists {
+		fmt.Fprintf(os.Stderr, "Warning: unable to enable bash-completion: %s does not exist\n", dirPath)
+		return nil // bash-completion isn't available, silently fail.
+	}
+
+	pathExists, err := fileutil.FileExists(path)
+	if err != nil {
+		return errors.Wrapf(err, "failed checking if %s exists", path)
+	}
+	if pathExists {
+		return nil // file already exists, don't update it.
+	}
+
+	// create the completion file
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	bashEntry, err := bashCompleteEntry()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Warning: unable to enable bash-completion: %s\n", err)
+		return nil // bash-completion isn't available, silently fail.
+	}
+
+	_, err = f.Write([]byte(bashEntry))
+	if err != nil {
+		return errors.Wrapf(err, "failed writing to %s", path)
+	}
+	return nil
+}
+
+func bashCompleteEntry() (string, error) {
+	return autocompleteBASH, nil
+}
---
+++ pkg/autocomplete/zsh.go
@@ -0,0 +1,119 @@
+package autocomplete
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/cockroachdb/errors"
+	"github.com/containerd/containerd/log"
+
+	"github.com/tensorchord/MIDI/pkg/util/fileutil"
+)
+
+var autocompleteZSH = `
+#compdef midi
+
+_cli_zsh_autocomplete() {
+
+  local -a opts
+  local cur
+  cur=${words[-1]}
+  if [[ "$cur" == "-"* ]]; then
+    opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} ${cur} --generate-bash-completion)}")
+  else
+    opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
+  fi
+
+  if [[ "${opts[1]}" != "" ]]; then
+    _describe 'values' opts
+  else
+    _files
+  fi
+
+  return
+}
+
+compdef _cli_zsh_autocomplete midi`
+
+// If debugging this, it might be required to run `rm ~/.zcompdump*` to remove the cache
+func InsertZSHCompleteEntry() error {
+	// should be the same on linux and macOS
+	path := "/usr/local/share/zsh/site-functions/_midi"
+	dirPath := filepath.Dir(path)
+
+	dirPathExists, err := fileutil.DirExists(dirPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to check if %s exists", dirPath)
+	}
+	if !dirPathExists {
+		log.L.Warnf("Warning: unable to enable zsh-completion: %s does not exist", dirPath)
+		return nil // zsh-completion isn't available, silently fail.
+	}
+
+	pathExists, err := fileutil.FileExists(path)
+	if err != nil {
+		return errors.Wrapf(err, "failed to check if %s exists", path)
+	}
+	if pathExists {
+		return nil // file already exists, don't update it.
+	}
+
+	// create the completion file
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	compEntry, err := zshCompleteEntry()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Warning: unable to enable zsh-completion: %s\n", err)
+		return nil // zsh-completion isn't available, silently fail.
+	}
+
+	_, err = f.Write([]byte(compEntry))
+	if err != nil {
+		return errors.Wrapf(err, "failed writing to %s", path)
+	}
+
+	return deleteZcompdump()
+}
+
+func zshCompleteEntry() (string, error) {
+	return autocompleteZSH, nil
+}
+
+func deleteZcompdump() error {
+	var homeDir string
+	sudoUser, found := os.LookupEnv("SUDO_USER")
+	if !found {
+		var err error
+		homeDir, err = os.UserHomeDir()
+		if err != nil {
+			return errors.Wrapf(err, "failed to lookup current user home dir")
+		}
+	} else {
+		currentUser, err := user.Lookup(sudoUser)
+		if err != nil {
+			return errors.Wrapf(err, "failed to lookup user %s", sudoUser)
+		}
+		homeDir = currentUser.HomeDir
+	}
+	files, err := os.ReadDir(homeDir)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read dir %s", homeDir)
+	}
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), ".zcompdump") {
+			path := filepath.Join(homeDir, f.Name())
+			err := os.Remove(path)
+			if err != nil {
+				return errors.Wrapf(err, "failed to remove %s", path)
+			}
+		}
+	}
+	return nil
+}
