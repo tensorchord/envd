cd2c0db5b04dce639eff2144ae4fbbe04291020e
Ce Gao <ce.gao@outlook.com>
2022-05-30 08:52:34 +0800

    fix(home): Remove afterall to avoid flaky tests (#215)
    * fix: Remove afterall
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * fix: Update
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- pkg/home/manager_test.go
+++ pkg/home/manager_test.go
@@ -15,27 +15,15 @@
 package home
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/adrg/xdg"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/tensorchord/envd/pkg/util/fileutil"
 )
 
 var _ = Describe("home manager", func() {
-	BeforeEach(func() {
-		// Cleanup the home cache.
-		Expect(Initialize()).NotTo(HaveOccurred())
-		m := GetManager()
-		Expect(fileutil.RemoveAll(m.(*generalManager).cacheStatusFile)).NotTo(HaveOccurred())
-	})
-	AfterEach(func() {
-		// Cleanup the home cache.
-		Expect(Initialize()).NotTo(HaveOccurred())
-		m := GetManager()
-		Expect(fileutil.RemoveAll(m.(*generalManager).cacheStatusFile)).NotTo(HaveOccurred())
-	})
 	When("initialized", func() {
 		It("should initialized successfully", func() {
 			Expect(Initialize()).NotTo(HaveOccurred())
@@ -44,8 +32,10 @@ var _ = Describe("home manager", func() {
 			Expect(m.ConfigFile()).To(Equal(filepath.Join(xdg.ConfigHome, "envd/config.envd")))
 		})
 		It("should return the cache status", func() {
+			Expect(os.RemoveAll(filepath.Join(xdg.CacheHome, "envd/cache.status"))).NotTo(HaveOccurred())
 			Expect(Initialize()).NotTo(HaveOccurred())
 			m := GetManager()
+			m.(*generalManager).cacheMap = make(map[string]bool)
 			Expect(m.Cached("test")).To(BeFalse())
 			Expect(m.MarkCache("test", true)).To(Succeed())
 			Expect(m.Cached("test")).To(BeTrue())
