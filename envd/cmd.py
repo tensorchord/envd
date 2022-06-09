e8fa80c8c77079cad0e374426bea1c7462706f52
Keming <kemingy94@gmail.com>
2022-06-10 01:00:42 +0800

    feat: add pypi package
    Signed-off-by: Keming <kemingy94@gmail.com>

---
+++ envd/cmd.py
@@ -0,0 +1,27 @@
+# Copyright 2022 The envd Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import subprocess
+from pkg_resources import resource_filename
+
+
+def envd():
+    path = resource_filename("envd", "bin/envd")
+    subprocess.run([path] + sys.argv[1:])
+
+
+def envd_ssh():
+    path = resource_filename("envd", "bin/envd-ssh")
+    subprocess.run([path] + sys.argv[1:])
