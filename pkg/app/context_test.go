0acc8556c9efa8ad370cc6867e2c44842c7f7ddb
Keming <kemingyang@tensorchord.ai>
2022-07-18 17:46:34 +0800

    fix ginkgo test
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- pkg/app/context_test.go
+++ pkg/app/context_test.go
@@ -27,7 +27,7 @@ import (
 var _ = Describe("home context", func() {
 	defaultContext := "default"
 	BeforeEach(func() {
-		Expect(home.Initialize()).NotTo(HaveOccurred())
+		Expect(home.Initialize()).To(Succeed())
 	})
 	When("check the default context", func() {
 		It("should found the default context", func() {
@@ -38,63 +38,62 @@ var _ = Describe("home context", func() {
 	})
 
 	Describe("add a new context", Ordered, func() {
-		Context("TCP context", func() {
-			contextName := "tcp-test"
-			builder := types.BuilderTypeTCP
-			socketAddr := "0.0.0.0:12345"
+		contextName := "tcp-test"
+		builder := types.BuilderTypeTCP
+		socketAddr := "0.0.0.0:12345"
 
+		BeforeAll(func() {
+			err := home.GetManager().ContextCreate(contextName, builder, socketAddr, true)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should found a new context", func() {
+			contexts, err := home.GetManager().ContextList()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contexts.Current).To(Equal(contextName))
+		})
+
+		Describe("connect buildkit through TCP", Ordered, func() {
+			name := "envd-buildkitd-tcp-test"
+			buildContext := "testdata/build-test"
+			dockerArgs := []string{
+				"run", "-d", "-p", "12345:8000", "--name", name,
+				"--security-opt", "seccomp=unconfined", "--security-opt", "apparmor=unconfined",
+				"moby/buildkit:rootless", "--addr", "tcp://0.0.0.0:8000", "--oci-worker-no-process-sandbox",
+			}
 			BeforeAll(func() {
-				err := home.GetManager().ContextCreate(contextName, builder, socketAddr, true)
+				cmd := exec.Command("docker", dockerArgs...)
+				err := cmd.Run()
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("should be able to build image with TCP context", func() {
+				args := []string{"envd.test", "--debug", "build", "--path", buildContext}
+				app := New()
+				err := app.Run(args)
 				Expect(err).NotTo(HaveOccurred())
 			})
 
 			AfterAll(func() {
-				err := home.GetManager().ContextUse(defaultContext)
-				Expect(err).NotTo(HaveOccurred())
-				err = home.GetManager().ContextRemove(contextName)
-				Expect(err).NotTo(HaveOccurred())
-				contexts, err := home.GetManager().ContextList()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(contexts.Current).To(Equal(defaultContext))
+				cmd := exec.Command("docker", "stop", name)
+				err := cmd.Run()
+				Expect(err).ToNot(HaveOccurred())
 			})
+		})
 
-			It("should found a new context", func() {
-				contexts, err := home.GetManager().ContextList()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(contexts.Current).To(Equal(contextName))
-			})
+		It("fail to delete the current context", func() {
+			err := home.GetManager().ContextRemove(contextName)
+			Expect(err).To(HaveOccurred())
+		})
 
-			When("connect buildkit through TCP", func() {
-				name := "envd-buildkitd-tcp-test"
-				BeforeEach(func() {
-					cmd := exec.Command("docker", "run", "-d", "-p", "12345:8000", "--name", name, "--security-opt", "seccomp=unconfined", "--security-opt", "apparmor=unconfined", "moby/buildkit:rootless", "--addr", "tcp://0.0.0.0:8000", "--oci-worker-no-process-sandbox")
-					err := cmd.Run()
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should be able to build image with TCP context", func() {
-					args := []string{"envd.test", "--debug", "build"}
-					app := New()
-					err := app.Run(args)
-					Expect(err).NotTo(HaveOccurred())
-				})
-
-				AfterEach(func() {
-					cmd := exec.Command("docker", "stop", name)
-					err := cmd.Run()
-					Expect(err).ToNot(HaveOccurred())
-				})
-			})
-
-			It("fail to delete the current context", func() {
-				err := home.GetManager().ContextRemove(contextName)
-				Expect(err).To(HaveOccurred())
-			})
-
-			It("switch to default context to delete test context", func() {
-				err := home.GetManager().ContextUse(defaultContext)
-				Expect(err).NotTo(HaveOccurred())
-			})
+		AfterAll(func() {
+			err := home.GetManager().ContextUse(defaultContext)
+			Expect(err).NotTo(HaveOccurred())
+			err = home.GetManager().ContextRemove(contextName)
+			Expect(err).NotTo(HaveOccurred())
+			contexts, err := home.GetManager().ContextList()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contexts.Current).To(Equal(defaultContext))
 		})
 	})
 })
