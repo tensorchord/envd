69a760250eb9df27a536b62d14fc3f6d312a0393
Qi Chen <aseaday@hotmail.com>
2022-07-26 13:50:00 +0800

    WIP: feature support robus tag and label for cache
    Signed-off-by: Qi Chen <aseaday@hotmail.com>

--- pkg/lang/frontend/starlark/interpreter.go
+++ pkg/lang/frontend/starlark/interpreter.go
@@ -15,6 +15,10 @@
 package starlark
 
 import (
+	"bytes"
+	"hash/fnv"
+	"io/ioutil"
+
 	"github.com/cockroachdb/errors"
 	"github.com/sirupsen/logrus"
 	"go.starlark.net/repl"
@@ -51,6 +55,30 @@ func NewInterpreter(buildContextDir string) Interpreter {
 		},
 		buildContextDir: buildContextDir,
 	}
+}
+
+func GetEnvdProgramHash(filename string) (uint64, error) {
+	envdSrc, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	// No Check builtin or predeclared for now
+	funcAlwaysHas := func(x string) bool {
+		return true
+	}
+	_, prog, err := starlark.SourceProgram(filename, envdSrc, funcAlwaysHas)
+	if err != nil {
+		return 0, err
+	}
+	buf := new(bytes.Buffer)
+	err = prog.Write(buf)
+	if err != nil {
+		return 0, err
+	}
+	h := fnv.New64a()
+	h.Write(buf.Bytes())
+	hashsum := h.Sum64()
+	return hashsum, nil
 }
 
 func (s generalInterpreter) ExecFile(filename string, funcname string) (interface{}, error) {
