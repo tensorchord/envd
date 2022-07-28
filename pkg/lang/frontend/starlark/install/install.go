b00d3553454e4bf1fcbe594dd826a58978d8e9e5
yqwu18 <yqwu18@iflytek.com>
2022-07-28 11:28:59 +0800

    add install conda yaml

--- pkg/lang/frontend/starlark/install/install.go
+++ pkg/lang/frontend/starlark/install/install.go
@@ -187,10 +187,10 @@ func ruleFuncVSCode(thread *starlark.Thread, _ *starlark.Builtin,
 func ruleFuncConda(thread *starlark.Thread, _ *starlark.Builtin,
 	args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
 	var name, channel *starlark.List
-	var envFile starlark.String
+	var env_file starlark.String
 
 	if err := starlark.UnpackArgs(ruleConda,
-		args, kwargs, "name", &name, "channel?", &channel, "envFile?", &envFile); err != nil {
+		args, kwargs, "name", &name, "channel?", &channel, "env_file?", &env_file); err != nil {
 		return nil, err
 	}
 
@@ -208,15 +208,15 @@ func ruleFuncConda(thread *starlark.Thread, _ *starlark.Builtin,
 		}
 	}
 	var path *string = nil
-	envFileStr := envFile.GoString()
+	env_filestr := env_file.GoString()
 	if envFileStr != "" {
 		buildContextDir := starlark.Universe[builtin.BuildContextDir]
 		buildContextDirStr := buildContextDir.(starlark.String).GoString()
-		buf := filepath.Join(buildContextDirStr, envFileStr)
+		buf := filepath.Join(buildContextDirStr, env_filestr)
 		path = &buf
 	}
 
-	logger.Debugf("rule `%s` is invoked, name=%v, channel=%v,envFile=%s", ruleConda, nameList, channelList, envFileStr)
+	logger.Debugf("rule `%s` is invoked, name=%v, channel=%v,envFile=%s", ruleConda, nameList, channelList, env_filestr)
 	ir.CondaPackage(nameList, channelList, path)
 
 	return starlark.None, nil
