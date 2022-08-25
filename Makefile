2c9497169928485efbb3b5c4eafb7268752a4315
Jinjing Zhou <VoVAllen@users.noreply.github.com>
2022-08-25 16:27:12 +0800

    test: add e2e test for examples in documentations (#804)
    * add
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>
    
    * fix
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>
    
    * lint
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>
    
    * add
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>
    
    * add
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>
    
    * remove blank line
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>
    
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

--- Makefile
+++ Makefile
@@ -181,6 +181,16 @@ e2e-test:
 		-X $(ROOT)/pkg/version.developmentFlag=true" \
 		-race -v -timeout 20m -coverpkg=./pkg/app -coverprofile=e2e-coverage.out ./e2e
 
+doc-test:
+	@go test -ldflags "-s -w -X $(ROOT)/pkg/version.version=$(VERSION) \
+		-X $(ROOT)/pkg/version.buildDate=$(BUILD_DATE) \
+		-X $(ROOT)/pkg/version.gitCommit=$(GIT_COMMIT) \
+		-X $(ROOT)/pkg/version.gitTreeState=$(GIT_TREE_STATE)                     \
+		-X $(ROOT)/pkg/version.gitTag="$(shell git describe --tags --abbrev=0)" \
+		-X $(ROOT)/pkg/version.developmentFlag=true" \
+		-race -v -timeout 20m -coverpkg=./pkg/app -coverprofile=e2e-coverage.out ./e2e/docs
+
+
 clean:  ## Clean the outputs and artifacts
 	@-rm -vrf ${OUTPUT_DIR}
 	@-rm -vrf ${DEBUG_DIR}
---
+++ e2e/docs/docs_test.go
@@ -0,0 +1,95 @@
+// Copyright 2022 The envd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package docs
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/tensorchord/envd/e2e"
+	"github.com/tensorchord/envd/pkg/app"
+	"github.com/tensorchord/envd/pkg/home"
+)
+
+var _ = Describe("check examples in documentation", func() {
+	buildContext := "doctest"
+	// env := "up-test"
+	baseArgs := []string{
+		"envd.test", "--debug",
+	}
+
+	BeforeEach(func() {
+		Expect(home.Initialize()).NotTo(HaveOccurred())
+		e2e.ResetEnvdApp()
+		envdApp := app.New()
+		err := envdApp.Run(append(baseArgs, "bootstrap"))
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("can list envd envs", func() {
+		envdApp := app.New()
+		err := envdApp.Run([]string{"envd.test", "--debug", "envs", "list"})
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("can check envd envs details", func() {
+		buildContext := "testdata/minimal"
+		args := append(baseArgs, []string{
+			"up", "--path", buildContext, "--detach", "--force",
+		}...)
+		e2e.ResetEnvdApp()
+		envdApp := app.New()
+		err := envdApp.Run(args)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = envdApp.Run([]string{"envd.test", "--debug", "envs", "describe"})
+		Expect(err).NotTo(HaveOccurred())
+
+		destroyArgs := append(baseArgs, []string{
+			"destroy", "--path", buildContext,
+		}...)
+		err = envdApp.Run(destroyArgs)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	up_tests := []string{"testdata/minimal", "testdata/getting_started", "testdata/jupyter", "testsdata/complex"}
+
+	for _, v := range up_tests {
+		It(fmt.Sprintf("can up %s environment", v), func() {
+			args := append(baseArgs, []string{
+				"up", "--path", "testdata/getting_started", "-f", "build.envd", "--detach", "--force",
+			}...)
+			e2e.ResetEnvdApp()
+			envdApp := app.New()
+			err := envdApp.Run(args)
+			Expect(err).NotTo(HaveOccurred())
+
+			destroyArgs := append(baseArgs, []string{
+				"destroy", "--path", buildContext,
+			}...)
+			err = envdApp.Run(destroyArgs)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	}
+
+})
+
+func TestMain(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "envd documentation example test suite")
+}
