236cd0be8cdfa9e4731d3b69cec7027001c8770b
Keming <kemingyang@tensorchord.ai>
2022-09-28 20:51:42 +0800

    fix: version tag in build.sh (#947)
    * fix: version tag in build.sh
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * rename to sshd
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * rm cuda inspect
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- base-images/build.sh
+++ base-images/build.sh
@@ -4,35 +4,30 @@ set -euo pipefail
 
 ROOT_DIR=`dirname $0`
 
-GIT_TAG_VERSION=$(git describe --tags --abbrev=0 | sed -r 's/[v]+//g') # remove v from version
-ENVD_VERSION="${ENVD_VERSION:-$GIT_TAG_VERSION}"
-DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG:-v$ENVD_VERSION}"
+GIT_TAG_VERSION=$(git describe --tags --abbrev=0)
 DOCKER_HUB_ORG="${DOCKER_HUB_ORG:-tensorchord}"
-PYTHON_VERSION="${PYTHON_VERSION:-3.9}"
 ENVD_OS="${ENVD_OS:-ubuntu20.04}"
 JULIA_VERSION="${JULIA_VERSION:-1.8rc1}"
 RLANG_VERSION="${RLANG_VERSION:-4.2}"
 
 cd ${ROOT_DIR}
 # ubuntu 22.04 build require moby/buildkit version greater than 0.8.1
-if ! docker buildx inspect cuda; then
-    docker buildx create --use --platform linux/x86_64,linux/arm64,linux/ppc64le --driver-opt image=moby/buildkit:v0.10.3
-fi
+docker buildx create --use --platform linux/x86_64,linux/arm64,linux/ppc64le --driver-opt image=moby/buildkit:v0.10.3
 
 # https://github.com/docker/buildx/issues/495#issuecomment-754688157
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # TODO(gaocegege): Support linux/arm64
 docker buildx build \
-    --build-arg ENVD_VERSION=${ENVD_VERSION} \
-    --build-arg ENVD_SSH_IMAGE=tensorchord/envd-sshd-from-scratch \
-    -t ${DOCKER_HUB_ORG}/r-base:${RLANG_VERSION}-envd-${DOCKER_IMAGE_TAG} \
+    --build-arg ENVD_VERSION=${GIT_TAG_VERSION} \
+    --build-arg ENVD_SSHD_IMAGE=tensorchord/envd-sshd-from-scratch \
+    -t ${DOCKER_HUB_ORG}/r-base:${RLANG_VERSION}-envd-${GIT_TAG_VERSION} \
     --pull --push --platform linux/x86_64 \
     -f r${RLANG_VERSION}.Dockerfile .
 docker buildx build \
-    --build-arg ENVD_VERSION=${ENVD_VERSION} \
-    --build-arg ENVD_SSH_IMAGE=tensorchord/envd-sshd-from-scratch \
-    -t ${DOCKER_HUB_ORG}/julia:${JULIA_VERSION}-${ENVD_OS}-envd-${DOCKER_IMAGE_TAG} \
+    --build-arg ENVD_VERSION=${GIT_TAG_VERSION} \
+    --build-arg ENVD_SSHD_IMAGE=tensorchord/envd-sshd-from-scratch \
+    -t ${DOCKER_HUB_ORG}/julia:${JULIA_VERSION}-${ENVD_OS}-envd-${GIT_TAG_VERSION} \
     --pull --push --platform linux/x86_64,linux/arm64 \
     -f julia${JULIA_VERSION}-${ENVD_OS}.Dockerfile .
 cd - > /dev/null
