9b091d989b356572f22f57ae225f20d0d41b6c97
Jinjing.Zhou <allenzhou@tensorchord.ai>
2022-09-29 16:53:21 +0800

    license
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

--- examples/mnist/main.py
+++ examples/mnist/main.py
@@ -1,3 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+# from https://github.com/pytorch/examples/blob/main/mnist/main.py
+
 from __future__ import print_function
 import argparse
 import torch
