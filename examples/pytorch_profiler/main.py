4b0bb4168069c35b3ecde49eacaad0a61ad10d18
Jinjing.Zhou <allenzhou@tensorchord.ai>
2022-10-14 13:19:29 +0800

    lint
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

--- examples/pytorch_profiler/main.py
+++ examples/pytorch_profiler/main.py
@@ -10,17 +10,19 @@ import torchvision.transforms as T
 from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 
 transform = T.Compose(
-    [T.Resize(224),
-     T.ToTensor(),
-     T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    [T.Resize(224), T.ToTensor(), T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+train_set = torchvision.datasets.CIFAR10(
+    root="./data", train=True, download=True, transform=transform
+)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=2, shuffle=True)
 
 device = torch.device("cuda:0")
-model = efficientnet_b0(weights = EfficientNet_B0_Weights.DEFAULT).to(device)
+model = efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT).to(device)
 criterion = torch.nn.CrossEntropyLoss().cuda(device)
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 model.train()
+
 
 def train(data):
     inputs, labels = data[0].to(device=device), data[1].to(device=device)
@@ -30,12 +32,15 @@ def train(data):
     loss.backward()
     optimizer.step()
 
+
 with torch.profiler.profile(
-        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
-        on_trace_ready=torch.profiler.tensorboard_trace_handler('/home/envd/log/efficientnet'),
-        record_shapes=True,
-        profile_memory=True,
-        with_stack=True
+    schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+    on_trace_ready=torch.profiler.tensorboard_trace_handler(
+        "/home/envd/log/efficientnet"
+    ),
+    record_shapes=True,
+    profile_memory=True,
+    with_stack=True,
 ) as prof:
     for step, batch_data in enumerate(train_loader):
         if step >= (1 + 1 + 3) * 2:
@@ -45,10 +50,13 @@ with torch.profiler.profile(
 
 
 prof = torch.profiler.profile(
-        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
-        on_trace_ready=torch.profiler.tensorboard_trace_handler('/home/envd/log/efficientnet'),
-        record_shapes=True,
-        with_stack=True)
+    schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+    on_trace_ready=torch.profiler.tensorboard_trace_handler(
+        "/home/envd/log/efficientnet"
+    ),
+    record_shapes=True,
+    with_stack=True,
+)
 prof.start()
 for step, batch_data in enumerate(train_loader):
     if step >= (1 + 1 + 3) * 2:
