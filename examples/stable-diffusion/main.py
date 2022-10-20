8cbd73184158054c0472f9d7f63b11cd9044036e
Ce Gao <cegao@tensorchord.ai>
2022-10-20 18:04:49 +0800

    fix: Fix format
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- examples/stable-diffusion/main.py
+++ examples/stable-diffusion/main.py
@@ -6,14 +6,17 @@ from diffusers import StableDiffusionPipeline
 
 device = "cuda"
 
+
 def dummy(images, **kwargs):
     return images, False
+
 
 # Read prompt from command line
 prompt = " ".join(sys.argv[1:])
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4",
-                                                use_auth_token=os.environ['HUGGINGFACE_TOKEN'])
+pipe = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4", use_auth_token=os.environ["HUGGINGFACE_TOKEN"]
+)
 pipe.to(device)
 pipe.safety_checker = dummy
 
@@ -21,4 +24,4 @@ pipe.safety_checker = dummy
 while True:
     n = random.randint(1000, 9999)
     image = pipe(prompt, guidance_scale=7.5)["sample"][0]
-    image.save(f'{n}.jpeg')
+    image.save(f"{n}.jpeg")
