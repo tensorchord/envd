75a727294074da46c65b9580bcbc4290faa0daf4
Jinjing.Zhou <allenzhou@tensorchord.ai>
2022-10-21 01:13:03 +0800

    fix
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import distutils.command.sdist
 import os
 import shutil
 import setuptools.command.sdist
