739c33949ca40193ac6a908ddbabd694052aba8a
Keming <kemingyang@tensorchord.ai>
2022-11-04 14:20:30 +0800

    fix: cuda tag in image cache
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- base-images/remote-cache/build-and-push-remote-cache.sh
+++ base-images/remote-cache/build-and-push-remote-cache.sh
@@ -27,8 +27,5 @@ TAG_SUFFIX="${TAG_SUFFIX:-}"
 cd ${ROOT_DIR}
 
 envd --debug build -f build.envd:${BUILD_FUNC} --export-cache type=registry,ref=docker.io/${DOCKER_HUB_ORG}/python-cache:envd-v${ENVD_VERSION}${TAG_SUFFIX} --force
-# envd build -f build.envd:build_gpu_11_2 --export-cache type=registry,ref=docker.io/${DOCKER_HUB_ORG}/python-cache:envd-v${ENVD_VERSION}-cuda-11.2.0-cudnn-8 --force
-# envd build -f build.envd:build_gpu_11_3 --export-cache type=registry,ref=docker.io/${DOCKER_HUB_ORG}/python-cache:envd-v${ENVD_VERSION}-cuda-11.3.0-cudnn-8 --force
-# envd build -f build.envd:build_gpu_11_6 --export-cache type=registry,ref=docker.io/${DOCKER_HUB_ORG}/python-cache:envd-v${ENVD_VERSION}-cuda-11.6.0-cudnn-8 --force
 
 cd - > /dev/null
