e38e48f27db26772c82c77a84ddfe92d786b46d5
Keming <kemingyang@tensorchord.ai>
2022-11-23 17:40:22 +0800

    chore: update horust to support glibc 2.17
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- pkg/types/envd.go
+++ pkg/types/envd.go
@@ -39,7 +39,7 @@ const (
 	// image
 	PythonBaseImage = "ubuntu:20.04"
 	// supervisor
-	HorustImage      = "tensorchord/horust:v0.1.0"
+	HorustImage      = "tensorchord/horust:v0.2.0"
 	HorustServiceDir = "/etc/horust/services"
 	HorustLogDir     = "/var/log/horust"
 	// env
