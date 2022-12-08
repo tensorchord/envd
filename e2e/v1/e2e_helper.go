9c7fdcb385d6ae80f15985a85ef2657adf7601a6
Ce Gao <cegao@tensorchord.ai>
2022-12-08 11:25:41 +0800

    fix: Update
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- e2e/v1/e2e_helper.go
+++ e2e/v1/e2e_helper.go
@@ -123,7 +123,7 @@ func (e *Example) Exec(cmd string) (string, error) {
 func (e *Example) ExecRuntimeCommand(cmd string) (string, error) {
 	buildContext := e.BuildContextPath
 	args := []string{
-		"envd.test", "--debug", "run", "-p", buildContext, "--command", cmd,
+		"envd.test", "--debug", "exec", "-p", buildContext, "--command", cmd,
 	}
 
 	buffer := new(bytes.Buffer)
