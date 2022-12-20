b89d40258ac2ef1c35b1e6d12703a79b10a4f914
Frost Ming <mianghong@gmail.com>
2022-12-20 11:41:26 +0800

    Forcely write py2.py3-none wheels
    Signed-off-by: Frost Ming <mianghong@gmail.com>

--- setup.py
+++ setup.py
@@ -58,13 +58,8 @@ class EnvdBuildExt(build_ext):
 
 class bdist_wheel_abi3(bdist_wheel):
     def get_tag(self):
-        python, abi, plat = super().get_tag()
-
-        if python.startswith("cp"):
-            # on CPython, our wheels are abi3 and compatible back to 3.6
-            return "cp36", "abi3", plat
-
-        return python, abi, plat
+        *_, plat = super().get_tag()
+        return "py2.py3", "none", plat
 
 
 class SdistCommand(sdist):
