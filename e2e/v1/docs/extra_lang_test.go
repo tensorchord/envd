f61444dc8e70e516a9ba2db48a4127b191215f39
Botong Ou <richard97@g.ucla.edu>
2023-01-10 21:35:13 -0500

    Combine julia and rlang into extra_lang_test.go
    Signed-off-by: Botong Ou <richard97@g.ucla.edu>

--- e2e/v1/docs/extra_lang_test.go
+++ e2e/v1/docs/extra_lang_test.go
@@ -6,9 +6,17 @@ import (
 	e2e "github.com/tensorchord/envd/e2e/v1"
 )
 
-var _ = Describe("rlang", Ordered, func() {
+var _ = Describe("lang", Ordered, func() {
 	It("Should build rlang environment successfully", func() {
 		exampleName := "rlang"
+		testcase := "e2e-extra"
+		e := e2e.NewExample(e2e.BuildContextDirWithName(exampleName), testcase)
+		e.BuildImage(true)()
+		e.RunContainer()()
+		e.DestroyContainer()()
+	})
+	It("Should build Julia environment successfully", func() {
+		exampleName := "julia"
 		testcase := "e2e-extra"
 		e := e2e.NewExample(e2e.BuildContextDirWithName(exampleName), testcase)
 		e.BuildImage(true)()
