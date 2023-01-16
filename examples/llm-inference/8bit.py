098ab7cada6bef60410d9dbaea14ac97a222abe2
Jinjing.Zhou <allenzhou@tensorchord.ai>
2023-01-16 20:14:19 +0800

    fix lint
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

--- examples/llm-inference/8bit.py
+++ examples/llm-inference/8bit.py
@@ -5,12 +5,16 @@ name = "bigscience/bloom-3b"
 text = "Hello my name is"
 max_new_tokens = 20
 
-def generate_from_model(model, tokenizer):
-  encoded_input = tokenizer(text, return_tensors='pt')
-  output_sequences = model.generate(input_ids=encoded_input['input_ids'].cuda())
-  return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
 
-model_8bit = AutoModelForCausalLM.from_pretrained(name, device_map="auto", load_in_8bit=True)
+def generate_from_model(model, tokenizer):
+    encoded_input = tokenizer(text, return_tensors="pt")
+    output_sequences = model.generate(input_ids=encoded_input["input_ids"].cuda())
+    return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
+
+
+model_8bit = AutoModelForCausalLM.from_pretrained(
+    name, device_map="auto", load_in_8bit=True
+)
 tokenizer = AutoTokenizer.from_pretrained(name)
 
 print(generate_from_model(model_8bit, tokenizer))
--- examples/llm-inference/main.py
+++ examples/llm-inference/main.py
@@ -5,10 +5,16 @@ name = "bigscience/bloom-3b"
 text = "Hello my name is"
 max_new_tokens = 20
 
-def generate_from_model(model, tokenizer):
-  encoded_input = tokenizer(text, return_tensors='pt')
-  output_sequences = model.generate(input_ids=encoded_input['input_ids'].cuda())
-  return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
 
-pipe = pipeline(model=name, model_kwargs= {"device_map": "auto", "load_in_8bit": True}, max_new_tokens=max_new_tokens)
+def generate_from_model(model, tokenizer):
+    encoded_input = tokenizer(text, return_tensors="pt")
+    output_sequences = model.generate(input_ids=encoded_input["input_ids"].cuda())
+    return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
+
+
+pipe = pipeline(
+    model=name,
+    model_kwargs={"device_map": "auto", "load_in_8bit": True},
+    max_new_tokens=max_new_tokens,
+)
 print(pipe(text))
