7089bda5d8fb6d5d994777b4e83b3f3a96b62b09
Zhizhen He <hezhizhen.yi@gmail.com>
2023-05-03 12:53:16 +0800

    chore: update copyright (#1583)
    Signed-off-by: Zhizhen He <hezhizhen.yi@gmail.com>

--- e2e/v1/docs/docs_test.go
+++ e2e/v1/docs/docs_test.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The envd Authors
+// Copyright 2023 The envd Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	e2e "github.com/tensorchord/envd/e2e/v1"
+	"github.com/tensorchord/envd/e2e/v1"
 	"github.com/tensorchord/envd/pkg/app"
 	"github.com/tensorchord/envd/pkg/home"
 )
