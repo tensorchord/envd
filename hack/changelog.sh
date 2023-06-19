cca7c0041943fd4198f8e53ac58b666fcd45daa1
Yuan Tang <terrytangyuan@gmail.com>
2023-06-19 14:02:25 -0400

    chore: Remove dependabot upgrades from changelog. Fixes #1655
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -9,7 +9,7 @@ git tag -l 'v*' | sort -rV | while read last; do
   if [ "$tag" != "" ]; then
     echo "## $(git for-each-ref --format='%(refname:strip=2) (%(creatordate:short))' refs/tags/${tag})"
     echo
-    git_log='git --no-pager log --no-merges --invert-grep --grep=^\(build\|ci\|docs\|test\|chore\):'
+    git_log='git --no-pager log --no-merges --invert-grep --grep=^\(build\|ci\|docs\|test\|chore\|chore(deps)\):'
 	  $git_log --format=' * [%h](https://github.com/tensorchord/envd/commit/%H) %s' $last..$tag
 	  echo
 	  echo "### Contributors"
