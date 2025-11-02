086e5e3d5c44e38798f2f4819b9b15680c29f4ae
zhangwei <zhangwei24@apache.org>
2025-11-02 20:57:32 +0800

    feat(run): add GPU resource flags to run command
    Signed-off-by: zhangwei <zhangwei24@apache.org>

--- e2e/cli/run_test.go
+++ e2e/cli/run_test.go
@@ -22,7 +22,7 @@ var _ = Describe("run command", func() {
 		"envd.test", "--debug",
 	}
 
-	When("given the right arguments", func() {
+	BeforeAll(func() {
 		Expect(home.Initialize()).NotTo(HaveOccurred())
 		envdApp := app.New()
 		err := envdApp.Run(append(baseArgs, "bootstrap"))
@@ -35,30 +35,24 @@ var _ = Describe("run command", func() {
 		Expect(err).NotTo(HaveOccurred())
 		_, err = envdEngine.Destroy(context.TODO(), env)
 		Expect(err).NotTo(HaveOccurred())
-		envdApp = app.New()
-		// build env image
-		buildArgs := append(baseArgs, "build", "--tag", buildImageName, "--path", buildContext)
-		err = envdApp.Run(buildArgs)
-		Expect(err).NotTo(HaveOccurred())
-		// run env
-		runArgs := append(baseArgs, "run", "--image", buildImageName, "--name", env, "--detach")
-		err = envdApp.Run(runArgs)
-		Expect(err).NotTo(HaveOccurred())
-	})
 
-	AfterAll(func() {
-		Expect(home.Initialize()).NotTo(HaveOccurred())
-		envdApp := app.New()
-		e2e.ResetEnvdApp()
-		err := envdApp.Run([]string{"envd.test", "--debug", "bootstrap"})
-		Expect(err).NotTo(HaveOccurred())
-		_, err = docker.NewClient(context.TODO())
-		Expect(err).NotTo(HaveOccurred())
-		c := types.Context{Runner: types.RunnerTypeDocker}
-		opt := envd.Options{Context: &c}
-		envdEngine, err := envd.New(context.TODO(), opt)
-		Expect(err).NotTo(HaveOccurred())
-		_, err = envdEngine.Destroy(context.TODO(), env)
-		Expect(err).NotTo(HaveOccurred())
+	})
+	When("given the right arguments", func() {
+		It("should build, run and destroy successfully", func() {
+			// build env image
+			buildArgs := append(baseArgs, "build", "--tag", buildImageName, "--path", buildContext)
+			e2e.ResetEnvdApp()
+			envdApp := app.New()
+			err := envdApp.Run(buildArgs)
+			Expect(err).NotTo(HaveOccurred())
+			// run env
+			runArgs := append(baseArgs, "run", "--image", buildImageName, "--name", env, "--detach")
+			err = envdApp.Run(runArgs)
+			Expect(err).NotTo(HaveOccurred())
+			// destroy env
+			destroyArgs := append(baseArgs, "destroy", "--name", env)
+			err = envdApp.Run(destroyArgs)
+			Expect(err).NotTo(HaveOccurred())
+		})
 	})
 })
