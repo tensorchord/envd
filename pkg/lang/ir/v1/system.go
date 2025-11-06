a7e0d8691d8d24b17339a619889b7061e6f7dd21
Keming <kemingyang@tensorchord.ai>
2025-11-06 09:48:54 +0800

    install go when merge op is enabled
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- pkg/lang/ir/v1/system.go
+++ pkg/lang/ir/v1/system.go
@@ -187,6 +187,8 @@ func (g *generalGraph) compileLanguage(root llb.State) (llb.State, error) {
 			lang = g.installJulia(root)
 		case "rust":
 			lang = g.installRust(root, language.Version)
+		case "go":
+			lang = g.installGolang(root, language.Version)
 		}
 		langs = append(langs, lang)
 	}
